6aa8a64c0d5395c7b155e28befb3344c084b653a
rxfatalslash <aaronsanchezmenendez@gmail.com>
2023-01-24 19:59:58 +0100

    Update reloj.js

--- js/reloj.js
+++ js/reloj.js
@@ -1,5 +1,5 @@
 function reloj() {
-    var now = new Date();;
+    var now = new Date();
     var hora = now.getHours();
     var minuto = now.getMinutes();
     var segundo = now.getSeconds();
@@ -9,12 +9,12 @@ function reloj() {
         segundo = "0" + segundo;
     }
 
-    var str_minuto = new String();
+    var str_minuto = new String(minuto);
     if (str_minuto.length == 1) {
         minuto = "0" + minuto;
     }
 
-    var str_hora = new String();
+    var str_hora = new String(hora);
     if (str_hora.length == 1) {
         hora = "0" + hora;
     }
