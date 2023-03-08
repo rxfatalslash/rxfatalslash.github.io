5c0193f6c2af8681026a15ed88bb04d9caecd987
rxfatalslash <aaronsanchezmenendez@gmail.com>
2023-03-08 19:07:13 +0100

    Cambio de función

--- js/reloj.js
+++ js/reloj.js
@@ -5,17 +5,17 @@ const reloj = () => {
     var segundo = now.getSeconds();
 
     var str_segundo = new String(segundo);
-    if (str_segundo.length == 1) {
+    if (str_segundo.length === 1) {
         segundo = "0" + segundo;
     }
 
     var str_minuto = new String(minuto);
-    if (str_minuto.length == 1) {
+    if (str_minuto.length === 1) {
         minuto = "0" + minuto;
     }
 
     var str_hora = new String(hora);
-    if (str_hora.length == 1) {
+    if (str_hora.length === 1) {
         hora = "0" + hora;
     }
 
