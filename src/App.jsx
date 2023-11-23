f159f19f4110779594ff57272135339c9e1edf7b
Aarón Sánchez <aaronsanchezmenendez@gmail.com>
2023-11-23 12:55:38 +0000

    Solución color de texto en modo oscuro

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,9 @@ function App() {
     document.body.style.backgroundColor = dark;
     document.querySelector('#about-me').style.color = color === "#f5f5f5" ? "white" : "black";
     document.querySelector('#contacto').style.color = color === "#f5f5f5" ? "white" : "black";
+    document.querySelector('.nombre').style.color = color === "#f5f5f5"? "black" : "white";
+    document.querySelector('.email').style.color = color === "#f5f5f5"? "black" : "white";
+    document.querySelector('.mensaje').style.color = color === "#f5f5f5"? "black" : "white";
     document.querySelector('#proyectos').style.backgroundColor = color === "#f5f5f5" ? "#636363" : "#a8e4ff";
     document.querySelector('#proyTit').style.color = color === "#f5f5f5" ? "white" : "black";
     document.querySelector('#presentation').style.color = color === "#f5f5f5" ? "#bfbfbf" : "#757778";
@@ -246,18 +249,18 @@ function App() {
       </main>
 
       <footer className="flex flex-col items-center py-10">
-        <div id="contacto" className="w-[95%]">
+        <div id="contacto" className='w-[95%]'>
           <h1 className="text-[50px] text-center font-rising-sun font-semibold tracking-normal mx-10 rounded-sm">
             Contacto
           </h1>
-          <form className="mt-10" action="https://formsubmit.co/aaronsanchezmenendez@gmail.com" method="post" onChange={handleChange}>
+          <form id="form" className="mt-10" action="https://formsubmit.co/aaronsanchezmenendez@gmail.com" method="post" onChange={handleChange}>
             <div className="grid grid-cols-12">
               <div className="col-start-2 md:col-start-4 col-span-5 md:col-span-3 flex flex-col">
                 <label className="font-bold" htmlFor="nombre">Nombre</label>
                 <input
                   id="nombre"
                   name="nombre"
-                  className="h-[60px] px-4 rounded-lg mt-2 focus:outline-blue-400"
+                  className="nombre h-[60px] px-4 rounded-lg mt-2 focus:outline-blue-400"
                   type="text"
                   placeholder="John Doe"
                 />
@@ -267,7 +270,7 @@ function App() {
                 <input
                   id="email"
                   name="email"
-                  className={`${isValid ? "focus:outline-blue-400" : "outline-red-400"} h-[60px] px-4 rounded-lg mt-2`}
+                  className={`${isValid ? "focus:outline-blue-400" : "outline-red-400"} email h-[60px] px-4 rounded-lg mt-2`}
                   type="email"
                   placeholder="example@gmail.com"
                 />
@@ -279,7 +282,7 @@ function App() {
                 <textarea
                   id="mensaje"
                   name="mensaje"
-                  className="rounded-lg px-4 py-4 mt-2 focus:outline-blue-400"
+                  className="mensaje rounded-lg px-4 py-4 mt-2 focus:outline-blue-400"
                   placeholder="Escribe aquí tu mensaje..."
                   rows="5"
                 ></textarea>
@@ -299,7 +302,7 @@ function App() {
           </a>
           <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/aarón-sánchez-menéndez-36537810a/" className="w-[50px] lg:w-[100px] h-[50px] lg:h-[100px] text-white rounded-full bg-linkedin hover:bg-[#185a7a] inline-flex items-center justify-center transition-all duration-200 ease-in-out mx-10">
             <svg className="w-[25px] lg:w-[50px] h-[25px] lg:h-[50px]" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0,0,256,256">
-              <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none"><g transform="scale(8.53333,8.53333)"><path d="M24,4h-18c-1.105,0 -2,0.895 -2,2v18c0,1.105 0.895,2 2,2h18c1.105,0 2,-0.895 2,-2v-18c0,-1.105 -0.895,-2 -2,-2zM10.954,22h-2.95v-9.492h2.95zM9.449,11.151c-0.951,0 -1.72,-0.771 -1.72,-1.72c0,-0.949 0.77,-1.719 1.72,-1.719c0.948,0 1.719,0.771 1.719,1.719c0,0.949 -0.771,1.72 -1.719,1.72zM22.004,22h-2.948v-4.616c0,-1.101 -0.02,-2.517 -1.533,-2.517c-1.535,0 -1.771,1.199 -1.771,2.437v4.696h-2.948v-9.492h2.83v1.297h0.04c0.394,-0.746 1.356,-1.533 2.791,-1.533c2.987,0 3.539,1.966 3.539,4.522z"></path></g></g>
+              <g fill="#ffffff" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none"><g transform="scale(8.53333,8.53333)"><path d="M24,4h-18c-1.105,0 -2,0.895 -2,2v18c0,1.105 0.895,2 2,2h18c1.105,0 2,-0.895 2,-2v-18c0,-1.105 -0.895,-2 -2,-2zM10.954,22h-2.95v-9.492h2.95zM9.449,11.151c-0.951,0 -1.72,-0.771 -1.72,-1.72c0,-0.949 0.77,-1.719 1.72,-1.719c0.948,0 1.719,0.771 1.719,1.719c0,0.949 -0.771,1.72 -1.719,1.72zM22.004,22h-2.948v-4.616c0,-1.101 -0.02,-2.517 -1.533,-2.517c-1.535,0 -1.771,1.199 -1.771,2.437v4.696h-2.948v-9.492h2.83v1.297h0.04c0.394,-0.746 1.356,-1.533 2.791,-1.533c2.987,0 3.539,1.966 3.539,4.522z"></path></g></g>
             </svg>
           </a>
           <a target="_blank" rel="noreferrer" href="https://twitter.com/rxfatalslash" className="w-[50px] lg:w-[100px] h-[50px] lg:h-[100px] text-white rounded-full bg-black hover:bg-gray-700 inline-flex items-center justify-center transition-all duration-200 ease-in-out">
@@ -308,7 +311,7 @@ function App() {
               </defs>
               <g style={{stroke: 'none', strokeWidth: '0', strokeDasharray: 'none', strokeLinecap: 'butt', strokeLinejoin: 'miter', strokeMiterlimit: '10', fill: 'none', fillRule: 'nonzero', opacity: '1'}} transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
                 <polygon points="24.89,23.01 57.79,66.99 65.24,66.99 32.34,23.01 " style={{stroke: 'none', strokeWidth: '1', strokeDasharray: 'none', strokeLinecap: 'butt', strokeLinejoin: 'miter', strokeMiterlimit: '10', fill: 'white', fillRule: 'nonzero', opacity: '1'}} transform="  matrix(1 0 0 1 0 0) " />
-                <path d="M 72.349 0 H 17.651 C 7.902 0 0 7.902 0 17.651 v 54.699 C 0 82.098 7.902 90 17.651 90 h 54.699 C 82.098 90 90 82.098 90 72.349 V 17.651 C 90 7.902 82.098 0 72.349 0 z M 56.032 70.504 L 41.054 50.477 L 22.516 70.504 h -4.765 L 38.925 47.63 L 17.884 19.496 h 16.217 L 47.895 37.94 l 17.072 -18.444 h 4.765 L 50.024 40.788 l 22.225 29.716 H 56.032 z" style={{stroke: 'none', strokeWidth: '1', strokeDasharray: 'none', strokeLinecap: 'butt', strokeLinejoin: 'miter', strokeMiterlimit: '10', fill: 'white', fillRule: 'nonzero', opacity: '1'}} transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                <path d="M 72.349 0 H 17.651 C 7.902 0 0 7.902 0 17.651 v 54.699 C 0 82.098 7.902 90 17.651 90 h 54.699 C 82.098 90 90 82.098 90 72.349 V 17.651 C 90 7.902 82.098 0 72.349 0 z M 56.032 70.504 L 41.054 50.477 L 22.516 70.504 h -4.765 L 38.925 47.63 L 17.884 19.496 h 16.217 L 47.895 37.94 l 17.072 -18.444 h 4.765 L 50.024 40.788 l 22.225 29.716 H 56.032 z" style={{stroke: 'none', strokeWidth: '1', strokeDasharray: 'none', strokeLinecap: 'butt', strokeLinejoin: 'miter', strokeMiterlimit: '10', fill: 'white', fillRule: 'nonzero', opacity: '1'}} transform=" matrix(1 0 0 1 0 0) " strokeLinecap="round" />
               </g>
             </svg>
           </a>
